e36b92a5881b08638010446daa0770304fff99ac
René <diaz.de.sandi.7@gmail.com>
2023-05-07 23:01:29 -0700

    create user

---
+++ nest-backend/README.md
@@ -0,0 +1,7 @@
+# Backend NestJS
+
+```
+docker compose up -d
+```
+
+Copiar ```.env.template``` y renombrarlo como ```.env```
---
+++ nest-backend/src/auth/auth.service.ts
@@ -0,0 +1,64 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+
+import * as bcryptjs from 'bcryptjs';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      // Pasos
+      // 1. Encriptar contraseña
+      const { password, ...userData } = createUserDto;
+
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
+
+      // 2. Guardar Usuario
+      // 3. Generar JWT
+
+      // Se elimina contraseña al responder la petición.
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+
+      return user;
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists`);
+      }
+      throw new InternalServerErrorException('Something bad happened!');
+    }
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateUserDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+}
