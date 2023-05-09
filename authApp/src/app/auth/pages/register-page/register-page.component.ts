0949ce99aba397b0345d0f5c3288af645d8855f3
René <diaz.de.sandi.7@gmail.com>
2023-05-08 22:52:58 -0700

    login | signup pages

---
+++ authApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.css'],
+})
+export class RegisterPageComponent {
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  signup() {
+    console.log(this.myForm.value);
+  }
+}
